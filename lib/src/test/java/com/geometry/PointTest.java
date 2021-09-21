3dc771781e8f1f733027cd7279d230173bebef53
kharismajd-gojek <made.j.interns@aux.gojek.com>
2021-09-22 06:36:20 +0800

    Added more testcase for distanceTo method for Point class

--- lib/src/test/java/com/geometry/PointTest.java
+++ lib/src/test/java/com/geometry/PointTest.java
@@ -7,10 +7,18 @@ import org.junit.jupiter.api.Test;
 public class PointTest {
 
     @Test
-    public void distanceToTestResult() {
+    public void distanceToTestResult1() {
         double answer = 0;
         Point point1 = new Point(0, 0);
         Point point2 = new Point(0, 0);
+        assertEquals(answer, point1.distanceTo(point2), 0.0001);
+    }
+
+    @Test
+    public void distanceToTestResult2() {
+        double answer = 5;
+        Point point1 = new Point(0, 0);
+        Point point2 = new Point(3, 4);
         assertEquals(answer, point1.distanceTo(point2), 0.0001);
     }
 }
